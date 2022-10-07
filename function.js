39bf58a559080905ac605275a55677848b389c0f
ReihanZakya <rezakya41@gmail.com>
2022-10-07 14:51:24 +0700

    humberger & create js function

---
+++ function.js
@@ -0,0 +1,43 @@
+// modal
+function myFunction(i) {
+    var box = document.getElementsByClassName('modal')[0];
+
+    if(i === 1) {
+        box.style.display = "block";
+    } else if (i === -1){
+        box.style.display = "none";
+    }
+};
+//    preloader
+var loader = document.getElementById("preloader");
+setTimeout(() => { loader.style.display = "none" 
+}, "1500");
+
+// gagal
+$(document).ready(function(){
+    $(".active-class a").click(function() {
+    $(".active-class a").removeClass("active");
+    $(this).addClass("active");
+    });
+
+    // hamburger
+    $('.icon').click(function(){
+        $('nav ul').slideToggle(100);
+        $('.burger').toggle();
+        $('.adios').toggle();
+    });
+
+    // tab
+    $('.tab_container:first').show();;
+           $('.tab_navigation li:first').addClass('active');;
+
+            $('.tab_navigation li').click(function(event){
+
+                index = $(this).index();
+                $('.tab_navigation li').removeClass('active');
+                $(this).addClass('active');
+                $('.tab_container').hide();
+                $('.tab_container').eq(index).show();
+
+            });
+});
